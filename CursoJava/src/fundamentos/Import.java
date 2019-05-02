7dce5fa65a887728bda54c1672a8b782c1349057
Andre Oliveira <Andre.Oliveira@la.logicalis.com>
2019-05-02 13:41:01 -0300

    Explorando o Import

---
+++ CursoJava/src/fundamentos/Import.java
@@ -0,0 +1,18 @@
+package fundamentos;
+
+import java.awt.Button;
+
+public class Import {
+	public static void main(String[] args) {
+		Double a = 2.0;
+		System.out.println(a);
+
+		Button b;
+
+		/*
+		 * Ctrl + Shift + O (Validar os imports e estruturar as necessecidades)
+		 * Ctrl + Shift + F (Identação do código)
+		 */
+
+	}
+}
