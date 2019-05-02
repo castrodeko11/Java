502c39e8a3e86a941836061dfb893d0ad77a6270
Andre Oliveira <Andre.Oliveira@la.logicalis.com>
2019-05-02 11:44:46 -0300

    Comentários em java

---
+++ CursoJava/src/fundamentos/Comentario.java
@@ -0,0 +1,27 @@
+package fundamentos;
+
+/**
+ * Classe responsável por mostrar o uso dos 3 tipos de comentários
+ * que o java suporta
+ * @author André Oliveira
+ * @version 1.0
+ * @since 2.2
+ *
+ */
+
+public class Comentario {
+	/**
+	 * Essa método pe a porta de entrada de um programa Java
+	 * 
+	 * @param args parâmentros por linha de comando
+	 */
+	public static void main(String[] args) {
+		/*
+		 * Imprime no console a palvra 'Primeiro' e em Seguida cria uma nova linha
+		 */
+		System.out.println("Primeiro");
+
+		// Cuidado com os comentários óbvios
+		System.out.println("Programa");
+	}
+}
