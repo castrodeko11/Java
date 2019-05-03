69210f7f44cc55a625a55e0c4aef70f428ebf293
Andre Oliveira <Andre.Oliveira@la.logicalis.com>
2019-05-03 10:22:06 -0300

    Conversão Numérica

---
+++ CursoJava/src/fundamentos/ConversaoNumerica.java
@@ -0,0 +1,21 @@
+package fundamentos;
+
+public class ConversaoNumerica {
+	public static void main(String[] args) {
+		// Exemplo 1 - Conversão explícita
+		float f = (float) 0.1;
+		System.out.println(f);
+		
+		// Exemplo 2 - Conversão explícita 
+		int i1 = (int) 7.9;
+		System.out.println(i1);
+		
+		// Exemplo 3 - Conversão implícita
+		int i2 = 'a';
+		System.out.println(i2);
+		
+		// Exemplo 4 = Conversão implícita 
+		double d = 1001;
+		System.out.println(d);
+	}
+}
