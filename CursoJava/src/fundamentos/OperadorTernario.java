fcbcde027b972ae089ccf35bd83db3151c2f1e3a
André Castro de Oliveira <castrodeko11@hotmail.com>
2019-05-05 15:54:28 -0300

    operador Ternario

---
+++ CursoJava/src/fundamentos/OperadorTernario.java
@@ -0,0 +1,11 @@
+package fundamentos;
+
+public class OperadorTernario {
+
+	public static void main(String[] args) {
+		double nota = 7.1;
+		String resultado = nota >= 7 ? "Aprovado" : "Recuperação";	
+		System.out.println(resultado);
+
+	}
+}
--- CursoJava/src/fundamentos/OperadoresUnarios.java
+++ CursoJava/src/fundamentos/OperadoresUnarios.java
@@ -15,6 +15,13 @@ public class OperadoresUnarios {
 		System.out.println(++num1 == num2--);
 		System.out.println(num1 == num2);
 		System.out.println(num2);
+		
+		// Complemento binário
+		System.out.println(Integer.toBinaryString(50));
+		System.out.println(Integer.toBinaryString(~50));
+		
+		// Negação
+		System.out.println(!true);
 	}
 
 }
