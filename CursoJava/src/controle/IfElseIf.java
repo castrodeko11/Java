0c8548a1cc061c8f4480ab07eb6dc7529a8e1b07
Andre Oliveira <Andre.Oliveira@la.logicalis.com>
2019-05-06 13:34:15 -0300

    If, Else e Else If

---
+++ CursoJava/src/controle/IfElseIf.java
@@ -0,0 +1,28 @@
+package controle;
+
+import java.util.Scanner;
+
+public class IfElseIf {
+	public static void main(String[] args) {
+		Scanner read = new Scanner(System.in);
+		double nota;
+
+		System.out.printf("Informe sua nota: ");
+		nota = read.nextDouble();
+
+		System.out.printf("Status de Aprovação: ");
+		if (nota >= 9) {
+			System.out.println("Quadro de Honra !");
+		} else if (nota >= 7) {
+			System.out.println("Aprovado");
+		} else if (nota >= 5.5) {
+			System.out.println("Recuperação");
+		} else if (nota >= 3.5) {
+			System.out.println("Recuperação + Trabalho");
+		} else {
+			System.out.println("Reprovado");
+		}
+
+	}
+
+}
