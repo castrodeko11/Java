ed0526c8cef1bb1714d018df018a48a8f351b2dd
Andre Oliveira <Andre.Oliveira@la.logicalis.com>
2019-05-27 09:11:58 -0300

    Variaveis Locais

---
+++ CursoJava/src/classe/VariaveisLocais.java
@@ -0,0 +1,33 @@
+package classe;
+
+public class VariaveisLocais {
+	int a = 2;
+	int a1 = 2;
+	
+	void teste1() {
+		int a1 =2;
+		System.out.println(a);
+		System.out.println(a1);
+		
+		
+		if (a1 > 3) {
+			int a2 = a1;
+			
+		}
+		
+//		System.out.println(a2);	
+		
+	}
+	
+	void teste2() {
+		int a1 =7;
+		System.out.println(this.a1);
+		int i = 0;
+		for (; i < 10; i++) {
+			
+		}
+		
+		System.out.println(i);
+	}
+
+}
